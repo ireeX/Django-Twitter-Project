f99629d5af07ecbef491f9c8eb9c9fa37054d5e9
Yuxuan Xie <iree.xie@gmail.com>
2021-12-15 19:10:23 -0500

    Add Likes Cancel API

--- likes/api/tests.py
+++ likes/api/tests.py
@@ -2,6 +2,7 @@ from utils.testcases import TestCase
 
 
 LIKE_BASE_URL = '/api/likes/'
+LIKE_CANCEL_URL = '/api/likes/cancel/'
 
 class LikeApiTests(TestCase):
     
@@ -94,3 +95,59 @@ class LikeApiTests(TestCase):
         response = self.user_client1.post(LIKE_BASE_URL, data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(comment.like_set.count(), 1)
+
+    def test_cancel(self):
+        tweet = self.create_tweet(self.user1)
+        comment = self.create_comment(self.user2, tweet)
+        like_comment_data = {'content_type': 'comment', 'object_id': comment.id}
+        like_tweet_data = {'content_type': 'tweet', 'object_id': tweet.id}
+        self.user_client1.post(LIKE_BASE_URL, like_comment_data)
+        self.user_client2.post(LIKE_BASE_URL, like_tweet_data)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # test unauthenticated cancel like
+        response = self.anonymous_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 403)
+
+        # test HTTP method
+        response = self.user_client1.get(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 405)
+
+        # test wrong content_type
+        response = self.user_client1.post(LIKE_CANCEL_URL, {
+            'content_type': 'xyz',
+            'object_id': 1,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        # test wrong object_id
+        response = self.user_client1.post(LIKE_CANCEL_URL, {
+            'content_type': 'comment',
+            'object_id': -1,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        # test cancel an non-exist like for comment
+        response = self.user_client2.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # test normal cancel like process for comment
+        response = self.user_client1.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 0)
+
+        #  test cancel an non-exist like for tweet
+        response = self.user_client1.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 0)
+
+        # test normal cancel like process for tweet
+        response = self.user_client2.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(tweet.like_set.count(), 0)
+        self.assertEqual(comment.like_set.count(), 0)
--- likes/api/views.py
+++ likes/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from likes.models import Like
 from likes.api.serializers import (
-    LikeSerializerForCreate,
     LikeSerializer,
+    LikeSerializerForCreate,
+    LikeSerializerForCancel,
 )
 from utils.decorators import required_params
 
@@ -31,3 +33,24 @@ class LikeViewSet(viewsets.GenericViewSet):
             LikeSerializer(instance).data,
             status=status.HTTP_201_CREATED,
         )
+
+    """
+    Cancel a like is different from delete a tweet or a comment.
+    It cannot depend on the id of the like. 
+    It should be bundled with the id of the liked object, the tweet or the comment.
+    """
+    @action(methods=['POST'], detail=False)
+    @required_params(method='POST', params=['content_type', 'object_id'])
+    def cancel(self, request):
+        serializer = LikeSerializerForCancel(
+            data=request.data,
+            context={'request': request}
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message:': 'Please check input.',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.cancel()
+        return Response({'success': True}, status=status.HTTP_200_OK)
