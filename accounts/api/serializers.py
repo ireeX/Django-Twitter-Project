b598ab2e45374abcc16d65691e92964c8d208662
Yuxuan Xie <iree.xie@gmail.com>
2021-12-22 16:21:31 -0500

    Add UserProfile model and change the display on Admin site

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -69,4 +69,6 @@ class SignupSerializer(serializers.ModelSerializer):
             email=email,
             password=password
         )
+        # Create UserProfile object
+        user.profile
         return user
