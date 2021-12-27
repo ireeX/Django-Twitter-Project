70b749a53009a4b5b44b9c28a2e15c5a8a7cd16f
Yuxuan Xie <iree.xie@gmail.com>
2021-12-26 23:40:55 -0500

    Upload Avatar

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework import exceptions
+from accounts.models import UserProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,21 +10,34 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email')
 
 
-class UserSerializerForTweet(serializers.ModelSerializer):
+class UserSerializerWithProfile(serializers.ModelSerializer):
+    # user.profile.nickname
+    nickname = serializers.CharField(source='profile.nickname')
+    avatar_url = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('id', 'username')
+        fields = ('id', 'username', 'nickname', 'avatar_url')
+
+    def get_avatar_url(self, obj):
+        if obj.profile.avatar:
+            return obj.profile.avatar.url
+        return None
+
+
+class UserSerializerForTweet(UserSerializerWithProfile):
+    pass
 
 
 class UserSerializerForFriendship(UserSerializerForTweet):
     pass
 
 
-class UserSerializerForComment(UserSerializerForTweet):
+class UserSerializerForComment(UserSerializerWithProfile):
     pass
 
 
-class UserSerializerForLike(UserSerializerForTweet):
+class UserSerializerForLike(UserSerializerWithProfile):
     pass
 
 
@@ -72,3 +86,9 @@ class SignupSerializer(serializers.ModelSerializer):
         # Create UserProfile object
         user.profile
         return user
+
+
+class UserProfileSerializerForUpdate(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('avatar', 'nickname')
