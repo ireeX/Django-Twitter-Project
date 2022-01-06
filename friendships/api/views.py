3a653a585e8174f7718ab84bdb6c815ed9bf7b0b
Yuxuan Xie <iree.xie@gmail.com>
2022-01-06 01:29:56 -0500

    Add pagination for friendships

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -9,30 +9,29 @@ from friendships.api.serializers import (
     FollowerSerializer,
     FriendshipSerializerForCreate,
 )
+from utils.pagination import FriendshipPagination
+
 
 class FriendshipViewSet(viewsets.GenericViewSet):
-    # Firendship module is based on users
+    # Friendship module is based on users
     queryset = User.objects.all()
     serializer_class = FriendshipSerializerForCreate
+    pagination_class = FriendshipPagination
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followers(self, request, pk):
         friendships = Friendship.objects.filter(to_user_id=pk).prefetch_related('from_user').order_by('-created_at')
-        serializer = FollowerSerializer(friendships, many=True)
-        return Response(
-            {'followers': serializer.data},
-            status=status.HTTP_200_OK
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowerSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
-    # url: /api/friendship/1/followings
+    # url: /api/friendship/1/followings/
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         friendships = Friendship.objects.filter(from_user_id=pk).prefetch_related('to_user').order_by('-created_at')
-        serializer = FollowingSerializer(friendships, many=True)
-        return Response(
-            {'followings': serializer.data},
-            status=status.HTTP_200_OK
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowingSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
@@ -117,8 +116,6 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             friendship.is_mutual = False
         friendship.save()
         return True
-
-
 
 
     # Another way is to use list() to perform the functions
