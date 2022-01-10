acd5ac2bff5eff93b40894b781d0c2037f7f79b7
Yuxuan Xie <iree.xie@gmail.com>
2022-01-10 16:55:43 -0500

    Apply Cache to Tweet

--- comments/models.py
+++ comments/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from tweets.models import Tweet
 from likes.models import Like
-from accounts.services import UserService
+from utils.memcached_helper import MemcachedHelper
 
 
 class Comment(models.Model):
@@ -27,7 +27,7 @@ class Comment(models.Model):
 
     @property
     def cached_user(self):
-        return UserService.get_user_through_cache(self.user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.user_id)
 
     def __str__(self):
         return '{} - {} comments {} at tweet {}'.format(
